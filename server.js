a9edd81a592ac36193c8037723b9f0057ab7e40a
Zachary Walker <walker@Zacharys-MacBook-Pro.local>
2022-02-01 15:28:51 -0500

    2nd

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
-app.get('/hello', (req, res) => {
-    res.json({message: 'Hello World'})
-})
 
 const PORT = process.env.PORT || 8081 
 
